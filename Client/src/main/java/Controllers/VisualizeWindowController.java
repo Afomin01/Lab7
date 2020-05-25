cd5b94db4800785b526561c4f1c2406760d3329c
artem <artemychmain@gmail.com>
2020-05-25 18:35:06 +0900

    Size changed

--- Client/src/main/java/Controllers/VisualizeWindowController.java
+++ Client/src/main/java/Controllers/VisualizeWindowController.java
@@ -42,62 +42,13 @@ public class VisualizeWindowController {
 
     private ObservableList<Route> items;
 
-    private int sizeX = 1000;
-    private int sizeY = 500;
+    private int sizeX = 1500;
+    private int sizeY = 600;
 
     @FXML
 
     void initialize() {
-        try {
-        Route route = new Route();
 
-        Path path = new Path();
-        path.setStrokeWidth(4.0);
-
-        CubicCurveTo cubicCurveTo = new CubicCurveTo();
-
-//        Location to = route.getTo();
-//        Location from = route.getFrom();
-
-        MoveTo moveTo = new MoveTo();
-        //moveTo.setX(from.getX());
-        //moveTo.setY(from.getY());
-
-        moveTo.setX(460.0f);
-        moveTo.setY(300.0f);
-
-        //int minX = Math.min(to.getX(), from.getX());
-        //int maxX = Math.max(to.getX(), from.getX());
-
-        //long minY = Math.min(to.getY(), from.getY());
-        //long maxY = Math.max(to.getY(), from.getY());
-
-        //cubicCurveTo.setControlX1((Math.random() * ((maxX - minX) + 1)) + minX);
-        //cubicCurveTo.setControlY1((Math.random() * ((maxY - minY) + 1)) + minY);
-        //cubicCurveTo.setControlX2((Math.random() * ((maxX - minX) + 1)) + minX);
-        //cubicCurveTo.setControlY2((Math.random() * ((maxY - minY) + 1)) + minY);
-
-        cubicCurveTo.setControlX1(200.0f);
-        cubicCurveTo.setControlX2(322.2f);
-        cubicCurveTo.setControlY1(50.0f);
-        cubicCurveTo.setControlY2(600.0f);
-
-
-        //cubicCurveTo.setX(to.getX());
-        //cubicCurveTo.setY(to.getY());
-
-        cubicCurveTo.setX(355.5f);
-        cubicCurveTo.setY(40.0f);
-
-        path.getElements().add(moveTo);
-        path.getElements().add(cubicCurveTo);
-
-        AnchorPane.setLeftAnchor(path, 15.0);
-
-        anchor.getChildren().add(path);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
     }
 
     public void setUpVisual(ObservableList<Route> list){
@@ -143,14 +94,14 @@ public class VisualizeWindowController {
         MoveTo moveTo = new MoveTo();
 
 
-        int toX = (int) (((to.getX() / Math.pow(10, Math.max(0, (int)(Math.log(to.getX())/Math.log(10) - 2))))) / 2);
-        int fromX = (int) (((from.getX() / Math.pow(10, Math.max(0, (int)(Math.log(from.getX())/Math.log(10) - 2))))) / 2);
+        int toX = (int) (((to.getX() / Math.pow(10, Math.max(0, (int)(Math.log(to.getX())/Math.log(10) - 2))))) / (2000/sizeX));
+        int fromX = (int) (((from.getX() / Math.pow(10, Math.max(0, (int)(Math.log(from.getX())/Math.log(10) - 2))))) / (2000/sizeX));
 
         int minX = Math.min(toX, fromX)+sizeX/2;
         int maxX = Math.max(toX, fromX)+sizeX/2;
 
-        long fromY = (long) (((from.getY() / Math.pow(10, Math.max(0, (int)(Math.log(from.getY())/Math.log(10) - 2))))) / 4);
-        long toY = (long) (((to.getY() / Math.pow(10, Math.max(0, (int)(Math.log(to.getY())/Math.log(10) - 2))))) / 4);
+        long fromY = (long) (((from.getY() / Math.pow(10, Math.max(0, (int)(Math.log(from.getY())/Math.log(10) - 2))))) / (2000/sizeY));
+        long toY = (long) (((to.getY() / Math.pow(10, Math.max(0, (int)(Math.log(to.getY())/Math.log(10) - 2))))) / (2000/sizeY));
 
         long minY = Math.min(fromY, toY)+sizeY/2;
         long maxY = Math.max(fromY, toY)+sizeY/2;
@@ -166,18 +117,12 @@ public class VisualizeWindowController {
         cubicCurveTo.setX(toX+sizeX/2);
         cubicCurveTo.setY(toY+sizeY/2);
 
-        System.out.println(toX);
-        System.out.println(toY);
-        System.out.println(fromX);
-        System.out.println(fromY);
-        System.out.println("next");
-
         path.getElements().add(moveTo);
         path.getElements().add(cubicCurveTo);
 
         Circle circle = new Circle();
-        circle.setCenterX(toX+sizeX/2);
-        circle.setCenterY(toY+sizeY/2);
+        circle.setCenterX(fromX+sizeX/2);
+        circle.setCenterY(fromY+sizeY/2);
         circle.setRadius(7.0D);
         circle.setFill(Color.MAGENTA);
         circle.setStrokeWidth(20.0D);
